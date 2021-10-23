ca3623548ea142dcd1806f332f33682746cff83c
Darren Hall <itsdarrenhall@gmail.com>
2021-10-23 10:37:14 +0100

    feat: initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,29 @@
+<main>
+  <slot></slot>
+</main>
+
+<style lang="scss">
+  @font-face {
+    font-family: 'Open Sans';
+    src: url('/fonts/open-sans-v16-latin-300.woff') format('woff'),
+        url('/fonts/open-sans-v16-latin-300.woff2') format('woff2');
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  :global(html) {
+    font-size: 1rem;
+
+    @media (min-width: 30em) {
+      font-size: 1.25rem;
+    }
+  } 
+
+  :global(body) {
+    background-color: #2f3132;
+    font-family: 'Open Sans', sans-serif;
+    line-height: 1.5;
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+/** @type {import('@sveltejs/kit').Config} */
+
+import preprocess from 'svelte-preprocess';
+
+const config = {
+  preprocess: preprocess(),
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;
